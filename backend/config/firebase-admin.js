c2ae540e6995080e8f91e4ad031d006b3c817422
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-07-20 00:14:00 +0600

    fix: call cert method directly from admin module namespace for v14 compatibility

--- backend/config/firebase-admin.js
+++ backend/config/firebase-admin.js
@@ -10,7 +10,7 @@ const initializeFirebase = () => {
         try {
           const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
           admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
+            credential: admin.cert(serviceAccount)
           });
           console.log('Firebase Admin initialized via JSON String.');
           isInitialized = true;
@@ -22,7 +22,7 @@ const initializeFirebase = () => {
       } else if (process.env.FIREBASE_PRIVATE_KEY) {
         try {
           admin.initializeApp({
-            credential: admin.credential.cert({
+            credential: admin.cert({
               projectId: process.env.FIREBASE_PROJECT_ID,
               clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
               privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
